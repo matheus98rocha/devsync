47c5b08817c6e8a822c9ffd47f9b8abe34a8ddde
matheus98rocha <matheus98rocha@gmail.com>
2023-06-21 21:11:41 -0300

    feat - theming

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="bg-white h-screen w-full">
+      <h1 className="text-black">Hello World</h1>
     </div>
   )
 }
