c8745de9ad9972ac472a7a66c4ab6691127d72d1
WellesleyMussolini <iei.muss@gmail.com>
2023-07-03 18:51:25 -0300

    Updated PrimaryButton styles

--- src/app/components/button/primary-button.component.tsx
+++ src/app/components/button/primary-button.component.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "./primary-button.types";
 
 const PrimaryButton = ({ buttonName, handleOnClick }: ButtonProps) => (
     <button
-        className="bg-primary text-white font-bold font-Gothic cursor-pointer outline-none border-none rounded text-white text-base w-40 h-12"
+        className="bg-primary text-white font-bold font-Gothic cursor-pointer outline-none border-none rounded text-base w-40 h-12"
         onClick={handleOnClick}>
         {buttonName}
     </button>
