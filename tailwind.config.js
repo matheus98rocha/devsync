4b1f289b612e8350e3669de3a84ddc33a665deb8
WellesleyMussolini <iei.muss@gmail.com>
2023-07-05 15:12:41 -0300

    Updated project theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,25 +6,33 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: "#008021",
-      primaryFocus: "#00681a",
-      error: "#FF8585",
-      alert: "#FFD485",
-      backgroud: "#fefefe",
-      contrastBackgroud: "#eaecec",
-      white: "#fff",
-      black: "#000",
-      gray: "#9b9b9b",
-      darkBlue: "#0676ed"
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#009F29",
+          hover: "#008021",
+          focus: "#00681a",
+        },
+        error: {
+          DEFAULT: "#FF8585",
+          hover: "#f14c4c"
+        },
+        alert: {
+          DEFAULT: "#FFD485",
+          hover: "#f4b61a",
+        },
+        disabled: {
+          DEFAULT: "#BBBBBB",
+          hover: "#AAAAAA",
+        },
+        background: "#fefefe",
+        contrastBackground: "#eaecec",
+        white: "#fff",
+        black: "#000",
+        gray: "#9b9b9b",
+        darkBlue: "#0676ed",
+      },
     },
   },
   plugins: [require("tailwindcss-animated")],
 };
-
-/*
-colors variations:
-primary: #11b811
-error: #f14c4c
-alert: "#f4b61a"
-*/
