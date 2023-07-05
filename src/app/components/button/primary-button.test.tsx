9749feaff65483e5ba1892e265b441f601017cdf
WellesleyMussolini <iei.muss@gmail.com>
2023-07-05 15:21:40 -0300

    Updated tests for PrimaryButton component

--- src/app/components/button/primary-button.test.tsx
+++ src/app/components/button/primary-button.test.tsx
@@ -1,24 +1,36 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"; 
+import "@testing-library/jest-dom/extend-expect";
 
 import PrimaryButton from "./primary-button.component";
 
 describe("PrimaryButton", () => {
-    const textButton = "Click Me";
+    const buttonText = "Click Me";
+    const buttonType = "primary";
 
     it("verifies if the button is rendered correctly", () => {
         const handleOnClick = jest.fn();
-        const { getByText } = render(<PrimaryButton textButton={textButton} handleOnClick={handleOnClick} />);
-
-        return expect(getByText(textButton)).toBeInTheDocument();
+        const { getByText } = render(
+            <PrimaryButton
+                buttonText={buttonText}
+                buttonType={buttonType}
+                handleOnClick={handleOnClick}
+            />
+        );
+        expect(getByText(buttonText)).toBeInTheDocument();
     });
 
     it("ensures that the handleOnClick function is called when the button is clicked", () => {
         const handleOnClick = jest.fn();
-        const { getByText } = render(<PrimaryButton textButton={textButton} handleOnClick={handleOnClick} />);
+        const { getByText } = render(
+            <PrimaryButton
+                buttonText={buttonText}
+                buttonType={buttonType}
+                handleOnClick={handleOnClick}
+            />
+        );
 
-        fireEvent.click(getByText(textButton));
-        return expect(handleOnClick).toHaveBeenCalledTimes(1);
+        fireEvent.click(getByText(buttonText));
+        expect(handleOnClick).toHaveBeenCalledTimes(1);
     });
 });
