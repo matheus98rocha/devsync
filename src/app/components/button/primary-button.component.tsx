e2848d24fdeb371d98c4bdc857e06884fd60d359
WellesleyMussolini <iei.muss@gmail.com>
2023-07-05 15:16:10 -0300

    Dynamically set button styles

--- src/app/components/button/primary-button.component.tsx
+++ src/app/components/button/primary-button.component.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { ButtonProps } from "./primary-button.types";
+import { buttonConfig } from "./config/buttonConfig";
 
-const PrimaryButton = ({ textButton, handleOnClick }: ButtonProps) => (
-    <button
-        className="bg-primary text-white font-bold cursor-pointer outline-none border-none rounded text-base w-40 h-12"
-        onClick={handleOnClick}>
-        {textButton}
-    </button>
-);
+const PrimaryButton = ({ buttonText, buttonType, handleOnClick }: ButtonProps) => {
+    const { buttonColor, buttonColorHover, cursor } = buttonConfig[buttonType];
+    return (
+        <button
+            className={`${buttonColor} ${buttonColorHover} duration-500 text-white font-bold ${cursor} outline-none border-none rounded text-base w-40 h-12`}
+            onClick={handleOnClick}
+        >
+            {buttonText}
+        </button>
+    );
+};
 
 export default PrimaryButton;
