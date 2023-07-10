14d79715489b8c03d4a7616cbfd9fc78e1b31bfa
WellesleyMussolini <iei.muss@gmail.com>
2023-07-10 17:39:38 -0300

    Facebook login authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,10 @@
 const { default: NextAuth } = require("next-auth/next");
 import GoogleProvider from "next-auth/providers/google";
-import FacebookProvider from "next-auth/providers/facebook";
 import GitHubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 
 const authOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string
-    }),
     GoogleProvider({
       clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
@@ -16,6 +12,10 @@ const authOptions = {
     FacebookProvider({
       clientId: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_SECRET as string
+    }),
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string
     }),
   ],
   secret: process.env.NEXT_PUBLIC_SECRET,
