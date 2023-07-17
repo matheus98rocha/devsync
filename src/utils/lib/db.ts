264462bdefaad9c0cc27137b99768806f15b5823
matheus98rocha <matheus98rocha@gmail.com>
2023-07-17 20:00:54 -0300

    Instaling dependencies

---
+++ src/utils/lib/db.ts
@@ -0,0 +1,14 @@
+import mongoose, { mongo } from "mongoose";
+
+let connection: typeof mongoose;
+
+const url = process.env.NEXT_DATABASE_URL as string;
+
+const startDb = async () => {
+  if (!connection) {
+    connection = await mongoose.connect(url);
+    return connection;
+  }
+}
+
+export startDb;
