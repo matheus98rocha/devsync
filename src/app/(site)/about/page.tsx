a344a9ad1d1f247a5eff761843aec6295b596864
WellesleyMussolini <iei.muss@gmail.com>
2023-07-21 09:01:51 -0300

    git collaborators

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -27,13 +27,13 @@ function About() {
           overflow-x-hidden
         "
       >
-        <h2>Sobre o que é o projeto ?</h2>
+        <h2 className='text-lg'>Sobre o que é o projeto ?</h2>
         <p className="indent-6">Nós somos uma equipe de desenvolvedores apaixonados por tecnologia e inovação. Estamos criando uma plataforma que busca simplificar e aprimorar funções já existentes e similares ao GitHub.</p>
         <p className="indent-6">Nosso objetivo é criar um ambiente onde programadores de todos os níveis possam se conectar, compartilhar conhecimentos e melhorar seus projetos.</p>
         <p className="indent-6">Queremos ser uma comunidade acolhedora, onde cada pessoa se sinta apoiada e encontre oportunidades para aprender e crescer. Acreditamos que colaboração e troca de ideias são fundamentais para o sucesso na programação.</p>
         <p className="indent-6">A missão do projeto é de poder capacitar a comunidade de desenvolvedores e criar um ambiente que inspire criatividade, aprendizado e inovação.</p>
         <div className='flex justify-center items-center flex-col gap-16'>
-          <h2>Equipe de desenvolvedores</h2>
+          <h2 className='text-lg'>Equipe de desenvolvedores</h2>
           <div className='flex flex-row gap-16'>
             {collaborators.map((user: CollaboratorsTypes, index: number) => <CollaboratorsCard keyValue={index} user={user.user} avatar={user.avatar} />)}
           </div>
