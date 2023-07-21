c78568da6bb1f8183a42a9a58cbfdc4fbad36753
WellesleyMussolini <iei.muss@gmail.com>
2023-07-21 08:53:41 -0300

    git collaborators

--- src/app/(site)/about/layout/cards/collaborators-card.component.tsx
+++ src/app/(site)/about/layout/cards/collaborators-card.component.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { CollaboratorsCardProps } from './collaborators-card.types';
 
 const CollaboratorsCard = ({ user, avatar, keyValue }: CollaboratorsCardProps) => (
-    <div
+    <a
+        href={`https://github.com/${user}`}
+        target="_blank"
+        className="flex justify-center items-center flex-col bg-white shadow-md rounded-lg p-4 w-52 cursor-pointer"
         key={keyValue}
-        className="bg-white shadow-md rounded-lg p-4 w-52"
     >
-        <a href={`https://github.com/${user}`} target="_blank" className="flex justify-center items-center flex-col">
-            <img
-                alt={user}
-                src={avatar}
-                className="w-24 h-24 rounded-full mb-2"
-            />
-            <h2 className="text-xl font-semibold">{user}</h2>
-        </a>
-    </div>
+        <img
+            alt={user}
+            src={avatar}
+            className="w-24 h-24 rounded-full mb-2"
+        />
+        <h2 className="text-xl font-semibold">{user}</h2>
+        {/* text-blue-500 hover:text-blue-700 duration-300 */}
+    </a>
 );
 
 export default CollaboratorsCard;
--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -27,14 +27,16 @@ function About() {
           overflow-x-hidden
         "
       >
-        <h1>Equipe de desenvolvimento</h1>
-        <h2>Quem somos ?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores illo non sed deserunt sapiente aspernatur vel reprehenderit nostrum expedita. Facere ducimus consectetur soluta ipsam ratione optio deserunt facilis, aspernatur assumenda.</p>
         <h2>Sobre o que é o projeto ?</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores illo non sed deserunt sapiente aspernatur vel reprehenderit nostrum expedita. Facere ducimus consectetur soluta ipsam ratione optio deserunt facilis, aspernatur assumenda.</p>
-        <h2>Nossas contas do GitHub</h2>
-        <div className='flex flex-row gap-16'>
-          {collaborators.map((user: CollaboratorsTypes, index: number) => <CollaboratorsCard keyValue={index} user={user.user} avatar={user.avatar} />)}
+        <p>Nós somos uma equipe de desenvolvedores apaixonados por tecnologia e inovação. Estamos criando uma plataforma que busca simplificar e aprimorar funções já existentes e similares ao GitHub.</p>
+        <p>Nosso objetivo é criar um ambiente onde programadores de todos os níveis possam se conectar, compartilhar conhecimentos e melhorar seus projetos.</p>
+        <p>Queremos ser uma comunidade acolhedora, onde cada pessoa se sinta apoiada e encontre oportunidades para aprender e crescer. Acreditamos que colaboração e troca de ideias são fundamentais para o sucesso na programação.</p>
+        <p>A missão do projeto é de poder capacitar a comunidade de desenvolvedores e criar um ambiente que inspire criatividade, aprendizado e inovação.</p>
+        <div className='flex justify-center items-center flex-col gap-16'>
+          <h2>Equipe de desenvolvedores</h2>
+          <div className='flex flex-row gap-16'>
+            {collaborators.map((user: CollaboratorsTypes, index: number) => <CollaboratorsCard keyValue={index} user={user.user} avatar={user.avatar} />)}
+          </div>
         </div>
       </div>
     </LoaggedUser>
