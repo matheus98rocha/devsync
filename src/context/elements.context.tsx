7d4e6273e8d976e874fb86ecc1bf513f89662e0f
matheus98rocha <matheus98rocha@gmail.com>
2023-07-26 22:22:54 -0300

    Feat - context mobile expand

--- src/context/elements.context.tsx
+++ src/context/elements.context.tsx
@@ -1,28 +1,41 @@
-'use client'
+"use client";
 import { ReactElement, createContext, useContext, useState } from "react";
 
 type ElementsContextState = {
   isOpenSidebar: boolean;
   toggleIsOpenSidebar: () => void;
+  isExpandedIconsMobile: boolean;
+  toggleIsExpandedIconsMobile: () => void;
 };
 
 type ElementsContextProps = {
   children: ReactElement;
 };
 
-const ElementsContext = createContext<ElementsContextState | undefined>(undefined);
+const ElementsContext = createContext<ElementsContextState | undefined>(
+  undefined
+);
 
 // Componente de Provedor do Contexto
 const ThemeProvider = ({ children }: ElementsContextProps) => {
   const [isOpenSidebar, setIsOpenSidebar] = useState<boolean>(false);
+  const [isExpandedIconsMobile, setIsExpandedIconsMobile] = useState(false);
 
   const toggleIsOpenSidebar = () => {
     setIsOpenSidebar((prevIsOpenSidebar) => !prevIsOpenSidebar);
   };
 
+  const toggleIsExpandedIconsMobile = () => {
+    setIsExpandedIconsMobile(
+      (prevIsExpandedIconsMobile) => !prevIsExpandedIconsMobile
+    );
+  };
+
   const contextValue: ElementsContextState = {
     isOpenSidebar,
     toggleIsOpenSidebar,
+    isExpandedIconsMobile,
+    toggleIsExpandedIconsMobile,
   };
 
   return (
