094063d38dc5e1d715f6764fd5ba9325dcc9777f
WellesleyMussolini <iei.muss@gmail.com>
2023-08-30 17:13:01 -0300

    fix: Adjust z-index for logout modal visibility
    Enhance the logout modal by adjusting its z-index value to ensure it appears above other components consistently. This modification improves the overlay and the modal effect.
    
    Author: Wellesley Mussolini

--- src/app/ui/layout/logout-modal/logout-modal.component.tsx
+++ src/app/ui/layout/logout-modal/logout-modal.component.tsx
@@ -5,36 +5,36 @@ import { GoAlertFill } from 'react-icons/go'
 const LogoutModal = ({ canShowlogoutModal, handleLogout, handleCancel }: LogoutModalProps) => {
   return (
     <>
-    {
-      canShowlogoutModal &&
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
-      <div className="fixed inset-0 bg-zinc-800 opacity-60 z-10" onClick={handleCancel}></div>
-      <div
-        className="relative z-20 gap-2 flex flex-col rounded-2xl border border-blue-100 bg-contrastBackground p-4 shadow-lg sm:p-6 lg:p-8 animate-jump-in animate-delay-[1ms]"
-        role="alert"
-      >
-        <div className="w-full flex items-center justify-center">
-          <GoAlertFill className="text-alert" size={45} />
+      {
+        canShowlogoutModal &&
+        <div className="z-50 fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+          <div className="fixed inset-0 bg-zinc-800 opacity-60 z-30" onClick={handleCancel}></div>
+          <div
+            className="relative z-40 gap-2 flex flex-col rounded-2xl border border-blue-100 bg-contrastBackground p-4 shadow-lg sm:p-6 lg:p-8 animate-jump-in animate-delay-[1ms]"
+            role="alert"
+          >
+            <div className="w-full flex items-center justify-center">
+              <GoAlertFill className="text-alert" size={45} />
+            </div>
+            <p className="mt-4 text-gray-500">Tem certeza de que deseja sair ?</p>
+            <div className="mt-6 flex flex-wrap gap-4 lg:grid lg:grid-cols-2 lg:gap-4">
+              <PrimaryButton
+                buttonText="Desconectar"
+                handleOnClick={handleLogout}
+                isDisabled={false}
+                buttonType="outlined"
+              />
+              <PrimaryButton
+                buttonText="Cancelar"
+                handleOnClick={handleCancel}
+                isDisabled={false}
+                buttonType="alert"
+              />
+            </div>
+          </div>
         </div>
-        <p className="mt-4 text-gray-500">Tem certeza de que deseja sair ?</p>
-        <div className="mt-6 flex flex-wrap gap-4 lg:grid lg:grid-cols-2 lg:gap-4">
-          <PrimaryButton
-            buttonText="Desconectar"
-            handleOnClick={handleLogout}
-            isDisabled={false}
-            buttonType="outlined"
-          />
-          <PrimaryButton
-            buttonText="Cancelar"
-            handleOnClick={handleCancel}
-            isDisabled={false}
-            buttonType="alert"
-          />
-        </div>
-      </div>
-    </div>
-  }
-  </>
+      }
+    </>
   )
 }
 
