0b53b1b1413951eba31df4efb7cc53fbf3369b0f
matheus98rocha <matheus98rocha@gmail.com>
2023-08-30 18:28:31 -0300

    feat: Componente de pesquisa

--- src/app/ui/components/search-input/search-input.component.tsx
+++ src/app/ui/components/search-input/search-input.component.tsx
@@ -1,42 +1,65 @@
+"use client";
 import React from "react";
 import { SearchProps } from "./search-input.types";
+import {
+  AiOutlineSearch,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
 
 function SearchInput({ searchValue, onChangeSearch }: SearchProps) {
+  const [isExpandedSearchInput, setIsPexpandedSearchInput] =
+    React.useState(false);
+
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsPexpandedSearchInput(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+
   return (
-    <div className="relative">
-      <label htmlFor="Search" className="sr-only">
-        Search{" "}
-      </label>
+    <div ref={ref}>
+      {isExpandedSearchInput ? (
+        <div className="relative animate-ease-linear duration-100">
+          <label htmlFor="Search" className="sr-only">
+            Search{" "}
+          </label>
 
-      <input
-        type="text"
-        id="Search"
-        placeholder="Pesquisar"
-        value={searchValue}
-        onChange={onChangeSearch}
-        className="w-full rounded-md border-gray-200 bg-contrastBackground py-2.5 ps-2 pe-10 shadow-sm sm:text-sm"
-      />
+          <input
+            type="text"
+            id="Search"
+            placeholder="Pesquisar"
+            value={searchValue}
+            onChange={onChangeSearch}
+            className="w-full rounded-md border-gray-200 bg-contrastBackground py-2.5 ps-2 pe-10 shadow-sm sm:text-sm"
+          />
 
-      <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
-        <button type="button" className="text-gray-600 hover:text-gray-700">
-          <span className="sr-only">Search</span>
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="h-4 w-4"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-            />
-          </svg>
-        </button>
-      </span>
+          <span className="absolute inset-y-0 end-0 grid w-10 place-content-center">
+            <button type="button" className="text-gray-600 hover:text-gray-700">
+              <span className="sr-only">Search</span>
+              <AiOutlineArrowLeft
+                onClick={() =>
+                  setIsPexpandedSearchInput(!isExpandedSearchInput)
+                }
+              />
+            </button>
+          </span>
+        </div>
+      ) : (
+        <div
+          className="bg-contrastBackground p-2 rounded-md cursor-pointer"
+          onClick={() => setIsPexpandedSearchInput(!isExpandedSearchInput)}
+        >
+          <AiOutlineSearch />
+        </div>
+      )}
     </div>
   );
 }
