aff42152d097c1e9e574354154f746253043a010
WellesleyMussolini <iei.muss@gmail.com>
2023-09-06 12:29:02 -0300

    feat: Loading component and mobile features
    Description:
    - Added a loading component to improve user experience on the About page.
    - Included detailed informations about the mobile version's feature of the project.
    
    Author: Wellesley Mussolini

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -1,11 +1,24 @@
 "use client";
 import React from "react";
 import LoaggedUser from "@/app/ui/layout/logged-user/logged-user";
-import { aboutMock, cardTexts } from "./mock/about.mock";
+import { aboutMock, cardTexts, mobileMock } from "./mock/about.mock";
 import AboutCard from "./components/card/about-card.component";
 import Loading from '@/app/ui/components/loading/loading.component'
 
 function About() {
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsLoading(true);
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <div className="h-screen w-screen flex items-center justify-center">
+        <Loading />
+      </div>
+    );
+  }
   return (
     <LoaggedUser>
       <div
@@ -27,22 +40,36 @@ function About() {
         "
       >
         <div className="flex flex-col gap-6 p-7">
-          <h1 className="text-center text-3xl mb-4 font-black text-gray">
+          <h1 className="text-center text-3xl mb-4 font-black text-disabled">
             A PLATAFORMA PARA TODOS OS DESENVOLVEDORES
           </h1>
-          {cardTexts.map((text, index) => (
-            <p key={index} className="text-center">
-              {text}
-            </p>
-          ))}
+          {cardTexts.map((text, index) => <p key={index} className="text-center">{text}</p>)}
         </div>
         <div className="flex flex-col items-center justify-center w-full gap-6">
-          <h2 className="uppercase font-black text-gray">
+          <h2 className="uppercase font-black text-disabled">
             objetivos do projeto
           </h2>
           <div className="px-8 grid grid-cols-1 xl:grid-cols-2 gap-10">
             {
               aboutMock.map((card, index) => (
+                <div key={index}>
+                  <AboutCard
+                    title={card.title}
+                    text={card.text}
+                    image={card.image}
+                  />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center w-full gap-6">
+          <h2 className="uppercase font-black text-disabled">
+            EXPANDINDO PARA VERSÃO MOBILE
+          </h2>
+          <div className="px-8 grid grid-cols-1 xl:grid-cols-2 gap-10">
+            {
+              mobileMock.map((card, index) => (
                 <div key={index}>
                   <AboutCard
                     title={card.title}
