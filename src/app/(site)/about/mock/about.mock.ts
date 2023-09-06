705040008105e74e8edf14cb8c2917350ec2f93e
WellesleyMussolini <iei.muss@gmail.com>
2023-09-06 12:33:43 -0300

    update: Mobile mock information
    Description:
    - Enhance the about's mock file with detailed mobile card's information.
    
    Author: Wellesley Mussolini

--- src/app/(site)/about/mock/about.mock.ts
+++ src/app/(site)/about/mock/about.mock.ts
@@ -23,3 +23,22 @@ export const cardTexts = [
   "Nós somos uma equipe de desenvolvedores apaixonados por tecnologia e inovação. Estamos criando uma plataforma que busca simplificar e aprimorar funções já existentes e similares ao GitHub.",
   "Nosso objetivo é criar um ambiente onde programadores de todos os níveis possam se conectar, compartilhar conhecimentos e melhorar seus projetos.",
 ];
+
+export const mobileMock = [
+  {
+    title: "Versão Mobile",
+    text: "A versão mobile é uma extensão natural da nossa visão de criar uma comunidade global de desenvolvedores conectados. Agora, você pode acessar a comunidade para onde quer que vá, direto no seu dispositivo móvel.",
+    image:
+      // "https://i.pinimg.com/736x/74/04/84/740484ef6ac2012bd9ef8abc42c340f6.jpg",
+      "https://i.pinimg.com/736x/26/b7/26/26b7266324d31907043236ac26c0a56c.jpg"
+  },
+  {
+    title: "INTERFACE INTUITIVA EM QUALQUER DISPOSITIVO",
+    text: "A nossa plataforma é projetada para ser intuitiva, independentemente do dispositivo que você usa. Tanto na versão desktop quanto na mobile, priorizamos a simplicidade e a facilidade de uso. Nossa interface móvel foi especialmente adaptada para dispositivos menores, garantindo uma experiência fluida e sem complicações em telas reduzidas.",
+    image:
+    // "https://icons8.com/wp-content/uploads/2020/03/social-media-logos-dotted.jpg",
+    // "https://c8.alamy.com/comp/R6AYB2/mobile-phone-to-smartphone-evolution-R6AYB2.jpg"
+    // "https://media.istockphoto.com/id/1136047141/vector/hand-drawn-sketch-of-mobile-phone.jpg?s=612x612&w=0&k=20&c=rc0brr_9Xz1xqfLhO4k61mKzcE4EPLE_bFz5XZCJ9SU="
+    "https://media.istockphoto.com/id/1160947798/vector/phone-evolution-hand-drawn-illustration.jpg?s=612x612&w=0&k=20&c=la0WWZZwuZ-52VoEBoG8cfVSLOmyKQIVDVK_1bJEwMM="
+  },
+];
