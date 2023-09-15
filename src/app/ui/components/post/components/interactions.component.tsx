2f28eeaa74a36649ccc8399e1249ba2c8ebf0685
WellesleyMussolini <iei.muss@gmail.com>
2023-09-15 13:13:57 -0300

    feat: Post interactions with hover effect
    Refactored the 'Post' component to componentize post interactions, including like, comment, share, and send functionalities. This change not only enhances the code but also adds a hover transition effect to the post interactions, improving user interactivity and visual feedback. It will make it easier to manage and customize post interactions.
    
    Author: Wellesley Mussolini

--- src/app/ui/components/post/components/interactions.component.tsx
+++ src/app/ui/components/post/components/interactions.component.tsx
@@ -4,21 +4,14 @@ import { postMock } from "../mock/post.mock";
 
 const Interactions = () => {
     const [isLiked, setIsLiked] = React.useState(false);
+    const CheckIsLiked = isLiked ? 'hover:cursor-pointer animate-jump text-primary' : 'hover:cursor-pointer animate-jump';
     return (
         <div className="w-full flex items-center justify-around my-2 border-t pt-5 border-[#d0d0d0]">
-            <div className="hover:cursor-pointer hover:text-gray duration-300">
+            <div className="hover:cursor-pointer hover:text-gray duration-300" onClick={() => setIsLiked(!isLiked)}>
                 {isLiked ? (
-                    <Bi.BiSolidLike
-                        className="hover:cursor-pointer animate-jump text-primary"
-                        size={25}
-                        onClick={() => setIsLiked(false)}
-                    />
+                    <Bi.BiSolidLike className={CheckIsLiked} size={25} />
                 ) : (
-                    <Bi.BiLike
-                        className="hover:cursor-pointer animate-jump"
-                        size={25}
-                        onClick={() => setIsLiked(true)}
-                    />
+                    <Bi.BiLike className={CheckIsLiked} size={25} />
                 )}
                 <p>Gostei</p>
             </div>
