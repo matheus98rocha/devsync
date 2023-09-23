83df2bf71a5e2f125a2d605170019f21155fe460
matheus98rocha <matheus98rocha@gmail.com>
2023-09-23 15:34:52 -0300

    feat: adding menu to sidebar

---
+++ src/app/ui/layout/logged-user/logger-user-desktop/sidebar/components/sidebar-menu/sidebar-menu.component.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SidebarMenu() {
+  return (
+    <div className="bg-background h-20 w-20 flex items-center justify-center">
+      <p>Teste menu</p>
+    </div>
+  );
+}
+
+export default SidebarMenu;
