464b99eaff3ce4ecdec85fe22e1d621b81fcdb3c
WellesleyMussolini <iei.muss@gmail.com>
2023-10-26 00:06:10 -0300

    refactor: Move post modal to a separate file

--- src/app/ui/layout/logged-user/logged-user.tsx
+++ src/app/ui/layout/logged-user/logged-user.tsx
@@ -11,31 +11,13 @@ import Sidebar from "./logger-user-desktop/sidebar/sidebar";
 import LogoutModal from "../logout-modal/logout-modal.component";
 import { signOut } from "next-auth/react";
 import LoadingFullScreen from "../../components/loading-full-screen/loading-full-screen";
-import PostModal from "../post-modal/post-modal.component";
-import PostButton from "./components/post-button/post-button.component";
 
-interface LoaggedUserProps {
-  children: React.ReactNode;
-  currentPage: string;
-}
-
-function LoaggedUser({ children, currentPage }: LoaggedUserProps) {
+function LoaggedUser({ children, currentPage }: { children: React.ReactNode, currentPage: string }) {
   const [showLogoutModal, setShowLogoutModal] = useState(false);
-  const [showPostModal, setShowPostModal] = useState(false);
-  const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
-  const [text, setText] = React.useState("");
-
   const { status } = useSession({
     required: true,
-    onUnauthenticated() {
-      redirect(`/?callbackUrl=/protected/${currentPage}`);
-    },
+    onUnauthenticated() { redirect(`/?callbackUrl=/protected/${currentPage}`) },
   });
-
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files && files[0]) setSelectedFile(files[0]);
-  };
 
   if (status === 'loading') return <LoadingFullScreen />;
   return (
@@ -45,20 +27,7 @@ function LoaggedUser({ children, currentPage }: LoaggedUserProps) {
         handleLogout={signOut}
         handleCancel={() => setShowLogoutModal(false)}
       />
-      <PostModal
-        canShowPostModal={showPostModal}
-        handleCloseModal={() => {
-          setShowPostModal(false);
-          setSelectedFile(null);
-          setText("");
-        }}
-        text={text}
-        handleText={setText}
-        handleFileChange={handleFileChange}
-        selectedFile={selectedFile}
-        handleSelectedFile={setSelectedFile}
-        characterLimit={300}
-      />
+
       <div className="h-screen w-screen flex items-center justify-center">
         {/* Mobile components*/}
         {isMobileOnly && <SidebarMobile />}
@@ -72,7 +41,6 @@ function LoaggedUser({ children, currentPage }: LoaggedUserProps) {
           <div className="w-screen h-full flex flex-col items-center justify-center">
             {children}
           </div>
-          <PostButton handlePostModalVisibility={setShowPostModal} />
         </Suspense>
       </div>
     </>
