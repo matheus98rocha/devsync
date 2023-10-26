30ee2ce410b6595e1813fd0ee6271e5c79036b3f
WellesleyMussolini <iei.muss@gmail.com>
2023-10-25 22:15:25 -0300

    feat: Enhance post modal with size adjustments, character limit, and remove icon
    Enhanced the post modal with the following improvements:
    
    - Increased the modal size.
    - Added a character limit counter, inspired by Twitter's character count feature.
    - Implemented a remove icon button for the file selection, allowing users to easily cancel or remove selected files when creating a post.
    
    Author: Wellesley Mussolini

--- src/app/ui/layout/post-modal/post-modal.component.tsx
+++ src/app/ui/layout/post-modal/post-modal.component.tsx
@@ -11,9 +11,13 @@ const PostModal = ({
     text,
     handleText,
     selectedFile,
+    handleSelectedFile,
     handleFileChange,
+    characterLimit,
 }: PostModalProps) => {
     const { data: session } = useSession();
+    const remainingCharacters = characterLimit - text.length;
+    const characterCounter = remainingCharacters < 0 ? 'text-red-500' : 'text-primary';
     return (
         <>
             {canShowPostModal && (
@@ -22,10 +26,11 @@ const PostModal = ({
                     <div
                         className="
                         relative 
-                        gap-5 
+                        gap-10
                         flex 
                         flex-col 
-                        rounded-2xl border 
+                        rounded-lg 
+                        border 
                         border-white 
                         bg-white 
                         p-4
@@ -34,45 +39,61 @@ const PostModal = ({
                         animate-delay-[1ms]
                         "
                         role="alert">
+
+                        {/* picture and user's name */}
                         <div className='flex w-full '>
                             <div className='flex items-center gap-2'>
                                 <Image
                                     src={session?.user?.image as string}
-                                    width={36}
-                                    height={36}
+                                    width={48}
+                                    height={48}
                                     priority={true}
                                     alt="use-profile"
                                     className="rounded-full"
                                 />
-                                <p
-                                    className='sm:text-lg text-sm'
-                                >{session?.user?.name as string}</p>
+                                <p className='sm:text-base text-sm'>{session?.user?.name as string}</p>
                             </div>
                             <AiOutlineClose
                                 className='absolute right-5 text-xl hover:text-gray duration-300 cursor-pointer'
                                 onClick={handleCloseModal} />
                         </div>
-                        <textarea
-                            rows={4}
-                            placeholder="No que você está pensando?"
-                            value={text}
-                            onChange={(e) => handleText(e.target.value)}
-                            className="border-none outline-none sm:w-[30rem] w-[14rem]"
-                        />
-                        <label
-                            className={`flex items-center ${selectedFile ? 'w-56 sm:w-64 pr-6' : 'w-14'} bg-[#f4f2ee] rounded-full cursor-pointer hover:shadow-postIcon duration-300`}>
-                            <input
-                                type="file"
-                                className="hidden"
-                                accept="image/*"
-                                onChange={handleFileChange}
+
+                        {/* text area */}
+                        <div className='flex flex-col gap-5'>
+                            <textarea
+                                placeholder="No que você está pensando?"
+                                value={text}
+                                onChange={(e) => handleText(e.target.value)}
+                                className="resize-none border-none outline-none sm:w-[40rem] sm:h-[15rem] w-[14rem] h-[8rem]"
                             />
-                            <div className={`text-2xl flex items-center justify-center h-14 w-14 ${selectedFile && 'pl-6'}`}><AiFillPicture /></div>
-                            <div className={`${selectedFile && "pl-3"} overflow-hidden`}>
-                                {selectedFile &&
-                                    <span className="whitespace-nowrap	overflow-hidden	text-ellipsis">{selectedFile["name"]}</span>}
+                            {/* characters limit per post */}
+                            <div className={`text-sm ${characterCounter}`}>
+                                {remainingCharacters} / {characterLimit}
                             </div>
-                        </label>
+                        </div>
+
+                        {/* file icon */}
+
+                        <div className={`flex items-center h-14 ${selectedFile ? 'w-56 sm:w-64 pr-6 gap-5 justify-between' : 'w-14 justify-center'} bg-[#f4f2ee] rounded-full cursor-pointer hover:shadow-postIcon duration-300`}>
+                            <label className={`flex items-center justify-center ${!selectedFile && "w-full h-full cursor-pointer"}`}>
+                                <input
+                                    type="file"
+                                    className="hidden"
+                                    accept="image/*"
+                                    onChange={handleFileChange}
+                                />
+                                <div className={`text-2xl flex items-center justify-center ${selectedFile && 'pl-6'}`}><AiFillPicture /></div>
+                            </label>
+                            <div className={`flex items-center justify-center overflow-hidden duration-300`}>
+                                {selectedFile &&
+                                    <span className="max-w-[128px] text-sm whitespace-nowrap overflow-hidden text-ellipsis">{selectedFile["name"]}</span>
+                                }
+                            </div>
+                            {selectedFile && <div> <AiOutlineClose className={"text-[1.3em] hover:text-gray duration-300"} onClick={() => handleSelectedFile(null)}/></div>}
+                        </div>
+
+
+                        {/* post button */}
                         <PrimaryButton
                             buttonContent="Publicar"
                             handleOnClick={() => alert("PUBLICADO")}
