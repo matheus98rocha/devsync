1961eadf543ab803368b2a43c8345847a27685ac
WellesleyMussolini <iei.muss@gmail.com>
2023-11-20 19:39:36 -0300

    chore: enhancement & refactor
    Streamlined post content code by making it smarter and reducing unnecessary lines. Introduce the use of alert components for improved feedback.
    
    Author: Wellesley Mussolini

--- src/app/(site)/home/components/post-content/post-content.component.tsx
+++ src/app/(site)/home/components/post-content/post-content.component.tsx
@@ -3,12 +3,17 @@
 import React from "react";
 import PostField from "../post-field/post-field.component";
 import PostModal from "@/app/ui/layout/post-modal/post-modal.component";
+import SucessfulAlert from "@/app/ui/components/alert/sucess/sucessful-alert.component";
+import ErrorAlert from "@/app/ui/components/alert/error/error-alert.component";
+import { handleAlertVisibility } from "@/utils/alert-visibility/alert-visibility";
 
 const PostContent = () => {
     const [text, setText] = React.useState<string>("");
+    const [showSucessfulAlert, setShowSucessfulAlert] = React.useState<boolean>(false);
+    const [showErrorAlert, setShowErrorAlert] = React.useState<boolean>(false);
     const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
     const [showPostModal, setShowPostModal] = React.useState(false);
-    
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (files && files[0]) setSelectedFile(files[0]);
@@ -18,17 +23,28 @@ const PostContent = () => {
         setText("");
         setShowPostModal(false);
     };
+
+    const handleSubmitPost = () => {
+        handleResetModal();
+        return handleAlertVisibility(setShowSucessfulAlert);
+    };
+
+    const handleError = () => handleAlertVisibility(setShowErrorAlert);
     return (
         <>
             <PostModal
                 canShowPostModal={showPostModal}
                 handleCloseModal={handleResetModal}
+                handleSubmitPost={handleSubmitPost}
                 handleFileChange={handleFileChange}
                 handleSelectedFile={setSelectedFile}
                 selectedFile={selectedFile}
                 handleText={setText}
                 text={text}
+                handleShowError={handleError}
             />
+            <ErrorAlert showAlert={showErrorAlert} message="ERROR AO PUBLICAR" />
+            <SucessfulAlert showAlert={showSucessfulAlert} message="ENVIADO COM SUCESSO" />
             <PostField handlePostModalVisibility={setShowPostModal} />
         </>
     )
