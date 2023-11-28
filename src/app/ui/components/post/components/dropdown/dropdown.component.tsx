5cc29444fcdcab0fe0b4458138baf777bb4695b5
WellesleyMussolini <iei.muss@gmail.com>
2023-11-28 19:24:29 -0300

    styles: added a rounded border on options

--- src/app/ui/components/post/components/dropdown/dropdown.component.tsx
+++ src/app/ui/components/post/components/dropdown/dropdown.component.tsx
@@ -9,7 +9,7 @@ const Dropdown = ({ handleDeleteModal, options }: {
             {options.map((option, index) => (
                 <li key={index} className="flex items-center justify-center px-4 py-2 w-[15rem] hover:bg-gray-100 cursor-pointer">
                     <div
-                        className="flex justify-center py-2 w-full items-center gap-2 hover:bg-primary-hover hover:text-white"
+                        className="flex justify-center py-2 w-full items-center gap-2 hover:bg-primary-hover hover:rounded hover:text-white"
                         onClick={() => handleDeleteModal(option.isClicked)}
                     >
                         <p>{option.icon}</p>
