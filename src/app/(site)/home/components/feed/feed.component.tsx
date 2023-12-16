aabe0c7a2d2af0c38de94b1d1df50dfae14f14b1
matheus98rocha <matheus98rocha@gmail.com>
2023-12-16 15:18:46 -0300

    Show time

--- src/app/(site)/home/components/feed/feed.component.tsx
+++ src/app/(site)/home/components/feed/feed.component.tsx
@@ -7,44 +7,35 @@ import { AlertEnum } from "@/app/ui/components/alert/alert.types";
 import Alert from "@/app/ui/components/alert/alert.component";
 import { FeedProps } from "./feed.types";
 import { handleAlertVisibility } from "@/utils/alert-visibility/alert-visibility";
-import { ICurrentUserPost, IPost } from "@/app/interfaces/post";
 import PostButton from "./components/post-button.component";
 import PostModal from "@/app/ui/layout/post-modal/post-modal.component";
+import { ICurrentUserPost, IPost } from "@/app/interfaces/post";
 
-const Feed = ({ posts, myUserId }: FeedProps) => {
-  const [text, setText] = React.useState<string>("");
-  const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
-  const [showErrorAlert, setShowErrorAlert] = React.useState<boolean>(false);
-  const [showPostModal, setShowPostModal] = React.useState<boolean>(false);
-  const [showSuccessfulNewPostAlert, setShowSuccessfulNewPostAlert] =
-    React.useState<boolean>(false);
-  const [showSuccessfulDeletedPostAlert, setSuccessfulDeletedPostAlert] =
-    React.useState<boolean>(false);
-  const [showDeletePostModal, setShowDeletePostModal] =
-    React.useState<boolean>(false);
-  const [isLoadingDeletePost, setIsLoadingDeletePost] =
-    React.useState<boolean>(false);
+const Feed = ({ name, image, posts, myUserId }: FeedProps) => {
+    const [text, setText] = React.useState<string>("");
+    const [selectedFile, setSelectedFile] = React.useState<File | null>(null);
+    const [showErrorAlert, setShowErrorAlert] = React.useState<boolean>(false);
+    const [showPostModal, setShowPostModal] = React.useState<boolean>(false);
+    const [showSuccessfulNewPostAlert, setShowSuccessfulNewPostAlert] = React.useState<boolean>(false);
+    const [showSuccessfulDeletedPostAlert, setSuccessfulDeletedPostAlert] = React.useState<boolean>(false);
+    const [showDeletePostModal, setShowDeletePostModal] = React.useState<boolean>(false);
+    const [isLoadingDeletePost, setIsLoadingDeletePost] = React.useState<boolean>(false);
 
-  const isMyPost: ICurrentUserPost[] = posts.map((post: IPost) => {
-    return {
-      id: post.id,
-      text: post.text,
-      image: post.author?.image || null || "", // use null if image is undefined
-      name: post.author?.name || null || "", // use null if name is undefined
-      currentUserPost: post.authorId === myUserId ? true : false, // it will check if the user logged made the post
+    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const files = event.target.files;
+        if (files && files[0]) setSelectedFile(files[0]);
     };
-  });
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files = event.target.files;
-    if (files && files[0]) setSelectedFile(files[0]);
-  };
+    const handleResetModal = () => {
+        setSelectedFile(null);
+        setText("");
+        setShowPostModal(false);
+    };
 
-  const handleResetModal = () => {
-    setSelectedFile(null);
-    setText("");
-    setShowPostModal(false);
-  };
+    const handleSubmitPost = async () => {
+        handleResetModal();
+        handleAlertVisibility(setShowSuccessfulNewPostAlert);
+    };
 
     const deletePost = async (id: string) => {
         setIsLoadingDeletePost(true);
@@ -57,25 +48,24 @@ const Feed = ({ posts, myUserId }: FeedProps) => {
     return (
         <>
             <PostButton buttonText="Começar uma publicação" handlePostModalVisibility={setShowPostModal} />
-            {posts.map((post: any, index: number) => (
-                <div className="w-full" key={index}>
-                    <Post
-                        key={index}
-                        text={post.text}
-                        image={post.image}
-                        name={post.name}
-                        handleDeleteModal={() => setShowDeletePostModal(true)}
-                        isMyPost={post.currentUserPost}
+            {posts.map((post: any) => (
+                    <div className="w-full" key={post.id}>
+                        <Post
+                            text={post.text}
+                            image={post.image}
+                            name={post.name}
+                            handleDeleteModal={() => setShowDeletePostModal(true)}
+                            isMyPost={post.currentUserPost}
 
-                        // delete post modal 
-                        deleteModalMessage="Deletar uma publicação é uma ação irreversível!"
-                        handleCancel={() => setShowDeletePostModal(false)}
-                        handleDelete={() => deletePost(post.id)}
-                        showDeleteModal={showDeletePostModal}
-                        isLoadingDeletePost={isLoadingDeletePost}
-                    />
-                </div>
-            )
+                            // delete post modal 
+                            deleteModalMessage="Deletar uma publicação é uma ação irreversível!"
+                            handleCancel={() => setShowDeletePostModal(false)}
+                            handleDelete={() => deletePost(post.id)}
+                            showDeleteModal={showDeletePostModal}
+                            isLoadingDeletePost={isLoadingDeletePost}
+                        />
+                    </div>
+                )
             )}
             <PostModal
                 canShowPostModal={showPostModal}
